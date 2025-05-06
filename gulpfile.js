f44f7a3b6dc5322c28b9046b0080945d4a818ea1
Declan W <declanwallace878@gmail.com>
2025-05-05 22:46:21 -0400

    Fix names for rules, fix comments, add final necessary gulp tasks in rules

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,8 @@ const stylelint = require(`gulp-stylelint`);
 const browserSync = require(`browser-sync`).create();
 const sourcemaps = require(`gulp-sourcemaps`);
 
-// Have to make validateHTML, validateCSS, validateJS, compressHTML, compressCSS, transpileJSForDev
-
 // Include ESLint
-const lintJS = () => {
+const validateJS = () => {
     return src(`app/js/app.js`)
         .pipe(eslint())
         .pipe(eslint.format())
@@ -18,7 +16,7 @@ const lintJS = () => {
 };
 
 // Include Stylelint
-const lintCSS = () => {
+const validateCSS = () => {
     return src(`app/css/style.css`)
         .pipe(
             stylelint({
@@ -27,7 +25,7 @@ const lintCSS = () => {
         );
 };
 
-//JavaScript to ES5
+// JavaScript to ES5
 const transpileJSForProd = () => {
     return src(`app/js/app.js`)
         .pipe(sourcemaps.init())
@@ -59,30 +57,65 @@ const compressCSS = () => {
         .pipe(dest(`prod/css`));
 };
 
-// Copy index.html to prod
-const copyHTML = () => {
-    return src(`app/html/index.html`).pipe(dest(`prod`));
+// Validate HTML
+const validateHTML = () => {
+    const htmlhint = require(`gulp-htmlhint`);
+    return src(`app/html/*.html`)
+        .pipe(htmlhint())
+        .pipe(htmlhint.reporter())
+        .pipe(htmlhint.failAfterError());
+};
+
+// Compress HTML
+const compressHTML = () => {
+    const htmlmin = require(`gulp-htmlmin`);
+    return src(`app/html/index.html`)
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(dest(`prod`));
+};
+
+// JavaScript to ES5 for Dev
+const transpileJSForDev = () => {
+    return src(`app/js/app.js`)
+        .pipe(sourcemaps.init())
+        .pipe(babel({ presets: [`@babel/preset-env`] }))
+        .pipe(sourcemaps.write(`.`))
+        .pipe(dest(`app/js`));
 };
 
 // Create Serve
 const serve = () => {
     browserSync.init({
         server: {
-            baseDir: [ `./app`,
+            baseDir: [
+                `./app`,
                 `./app/html`,
             ]
         },
     });
-    watch(`app/js/*.js`, series(lintJS, transpileJSForProd)).on(`change`, browserSync.reload);
-    watch(`app/css/*.css`, lintCSS).on(`change`, browserSync.reload);
+    watch(`app/js/*.js`, series(validateJS, transpileJSForProd)).on(`change`, browserSync.reload);
+    watch(`app/css/*.css`, validateCSS).on(`change`, browserSync.reload);
     watch(`app/html/*.html`).on(`change`, browserSync.reload);
 };
 
 // Export serve
-exports.serve = series(parallel(lintJS, lintCSS), transpileJSForProd, serve);
+exports.serve = series(
+    parallel(validateJS, validateCSS),
+    transpileJSForProd,
+    serve
+);
 
 // Set prod build as default
 exports.default = series(
-    parallel(lintJS, lintCSS),
-    parallel(compressJS, compressCSS, copyHTML)
+    parallel(validateJS, validateCSS),
+    parallel(compressJS, compressCSS, compressHTML)
 );
+
+exports.validateHTML = validateHTML;
+exports.validateCSS = validateCSS;
+exports.validateJS = validateJS;
+exports.compressHTML = compressHTML;
+exports.compressCSS = compressCSS;
+exports.compressJS = compressJS;
+exports.transpileJSForDev  = transpileJSForDev;
+exports.transpileJSForProd = transpileJSForProd;
